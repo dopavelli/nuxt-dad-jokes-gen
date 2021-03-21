8e353cd8d18039414a87f30cd0fb30c516bb179e
dopavelli <Sorc.2nd@gmail.com>
2021-03-22 00:58:50 +0100

    initial commit, basic setup

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Dad Jokes</h1>
+    <h3>Welcome, this is the HomePage</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Welcome",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
